bbd35e0076b19b07be71458927a0401d65d4d908
Yusra A <yusraa190@gmail.com>
2020-01-12 02:10:21 +0300

    update 4

--- Api/user/services/userservice.go
+++ Api/user/services/userservice.go
@@ -6,14 +6,17 @@ import (
 	"gitlab.com/username/carefirst/api/user"
 )
 
+// UserService hebdnmkf
 type UserService struct {
 	usRepo user.UsrService
 }
 
+// NewUserService bhedfnjgm
 func NewUserService(rep user.UsrRepository) *UserService {
 	return &UserService{usRepo: rep}
 }
 
+// User gets a user
 func (u *UserService) User(user *entity.User) (*entity.User, []error) {
 	us, errs := u.usRepo.User(user)
 	fmt.Println(errs)
@@ -21,4 +24,13 @@ func (u *UserService) User(user *entity.User) (*entity.User, []error) {
 		return nil, errs
 	}
 	return us, nil
+}
+
+//AuthUser ... checks username and password validity
+func (u *UserService) AuthUser(userName string, password string) (*entity.User, error) {
+	user, err := u.usRepo.AuthUser(userName, password)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
 }
